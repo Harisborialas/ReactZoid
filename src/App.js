e22f8614935db253cad9e8386e664b1e2abd1b5e
@Harisborialas <azan6743@gmail.com>
2023-04-14 15:27:17 +0500

    Zoid in React

--- src/App.js
+++ src/App.js
@@ -1,22 +1,20 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import { useState } from "react";
+import "./App.css";
 
-function App() {
+function App(props) {
+  let [myWord, changeMyWord] = useState("");
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Hello I'm a beautiful widget! My name is <code>{props.name || "undefined"}</code>
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <input placeholder="placeholder" />
+        <button onClick={() => props.passDownFunc(myWord)}>
+          Pass this word up to parent
+        </button>
       </header>
     </div>
   );
---
+++ src/widget.js
@@ -0,0 +1,8 @@
+import * as zoid from "zoid/dist/zoid.frameworks";
+let MyWidget = zoid.create({
+  tag: "my-widget",
+  url: "http://localhost:2000/index.html",
+});
+console.log(`yo! have loaded mywidget from child: ${MyWidget}`);
+
+export default MyWidget;
